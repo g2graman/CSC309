b890068631fd922298f3e57919b2a9197a76a150
Aashni Shah <contact@aashni.me>
2014-01-22 17:07:06 -0500

    codes not working. trying to run on cdf

--- SpaceInvaders/js/main.js
+++ SpaceInvaders/js/main.js
@@ -1,22 +1,40 @@
-<script>
   var lastUpdate = Date.now();
   var speed = 500.0;
   var roundCount = 1;
   var currentRound = 1;
   var notOver = true;
+
+  // call the main function
+
+  var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#000000";
+    context.fillRect(0,0,1024,768);
+
+
   function main() {
-    while(notOver)
-    run();
+      //alert("main");
+      run();
   };
   
   function run() {
-    var currentUpdate = Date.now();
-    var interval = (currentUpdate - lastUpdate) / speed;
+    //alert("run");
+    //var currentUpdate = Date.now();
+    //var interval = (currentUpdate - lastUpdate) / speed;
     
     //update();
-    //draw();
+    draw();
     
     lastUpdate = currentUpdate;
     
   };
-</script>
+
+  function draw(){
+    //alert("draw");
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#000000";
+    context.fillRect(0,0,1024,768);
+  };
+
+    main();
