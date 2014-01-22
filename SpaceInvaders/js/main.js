63b3c0c6964a5c6d2a70029192b44173ac5b4438
Aashni Shah <contact@aashni.me>
2014-01-22 17:56:34 -0500

    the tank and background exist, the keylogger is currently breaking the draw function somewhere

--- SpaceInvaders/js/main.js
+++ SpaceInvaders/js/main.js
@@ -4,30 +4,36 @@
   var currentRound = 1;
   var notOver = true;
 
-  // call the main function
+  var tankPosX = 497;
+  var tankPosY= 730;
 
-  var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");
-    context.fillStyle = "#000000";
-    context.fillRect(0,0,1024,768);
+  // call the main function
+  tank = new Object();
+  tank.posX = 497;
+  tank.posY = 730;
+  tank.onkeydown = tankKeyPress(key, tank);
+
+  main();
 
 
   function main() {
       //alert("main");
-      run();
+      //run();
+      setInterval(draw, speed);
   };
   
-  function run() {
+  /*function run() {
     //alert("run");
-    //var currentUpdate = Date.now();
-    //var interval = (currentUpdate - lastUpdate) / speed;
+    var currentUpdate = Date.now();
+    var interval = (currentUpdate - lastUpdate) / speed;
     
     //update();
     draw();
     
     lastUpdate = currentUpdate;
     
-  };
+  };*/
+
 
   function draw(){
     //alert("draw");
@@ -35,6 +41,25 @@
     var context = canvas.getContext("2d");
     context.fillStyle = "#000000";
     context.fillRect(0,0,1024,768);
+
+    //creating tank
+    context.fillStyle = "#FF0000";
+    context.fillRect(tank.posX,tank.posY,30,20);
   };
+
+ 
+
+  function tankKeyPress(key, tank){
+    // left = 37, right = 39
+    if(key.keyCode == 37 && tank.posX>0){
+      tank.posX = tank.posX - 1;
+    } else if(key.keyCode == 37 && tank.posX <= 0){
+      //tank.posX = tank.posX;
+    } else if(key.keyCode == 39 && tank.posX<994){
+      tank.posX = tank.posX + 1;
+    } else if(key.keyCode == 37 && tank.posX >= 994){
+      //tank.posX = tank.posX;
+    }
+  }
 
     main();
