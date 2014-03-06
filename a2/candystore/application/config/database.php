00febe9003862a37db2cd29e572d05739432de2e
Aashni Shah <contact@aashni.me>
2014-03-06 01:54:26 -0500

    login system doesn't work. reverting back to original code provided by prof

--- a2/candystore/application/config/database.php
+++ a2/candystore/application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'candystore_db';  
+$db['default']['hostname'] = 'bungle07.cs.toronto.edu';
+$db['default']['username'] = 'yourusernamegoeshere';
+$db['default']['password'] = 'yourpasswordgoedhere';
+$db['default']['database'] = 'yourdatabasenamegoeshere';  
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
