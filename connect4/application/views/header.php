1730bfc511a5b0ac291a41865e2b7a57279e6840
Aashni Shah <contact@aashni.me>
2014-04-04 04:17:27 -0400

    need to implement isWinner portion of game logic and need to deal with ending a game properly and returning back to the waiting area. implemented few bug fixes, and some basic styling

---
+++ connect4/application/views/header.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="<?= base_url() ?>/js/jquery.timers.js"></script>
+
+<!--   <link rel='stylesheet' type='text/css' href='<?= base_url() ?>css/board.css'> -->
+  <link rel='stylesheet' type='text/css' href='<?= base_url() ?>css/bootstrap.css'>
+  <link rel='stylesheet' type='text/css' href='<?= base_url() ?>css/flat-ui.css'>
+  <link rel='stylesheet' type='text/css' href='<?= base_url() ?>css/prettify.css'>
+  <link rel='stylesheet' type='text/css' href='<?= base_url() ?>css/our.css'>
+</head>
+
+<div class="container ourcenter">
