53133b9966531813128f0f30a9d7979370ce7b9c
Gramano Francesco R <g2graman@cdf.toronto.edu>
2014-04-04 16:35:27 -0400

    Got winner row working for horizontal axis, and other bug fixes. Yay.

--- connect4/application/views/match/board.php
+++ connect4/application/views/match/board.php
@@ -18,17 +18,19 @@
 		var boardCols = [[], [], [], [], [], [], []];
 
 		function Disk(player, row, col){
+			console.log('2');
 			this.playerid = player;
 			this.row = row;
 			this.col = col;
 
-			function getPlayerId() {
+			this.id = function getPlayerId() {
 				return this.playerid;
 			}
 		}
 
 		// player 1 makes a move
 		$(document).ready(function() {
+			console.log('3');
 			if(userId!=player1){
 				$('.tile').css('background-color', 'red');
 			} else {
@@ -44,6 +46,7 @@
 		});
 
 		function check_valid_move(item){
+			console.log('4');
 			var col = $(item).attr('id');
 			var row = boardCols[col].length;
 
@@ -56,6 +59,7 @@
 		}
 
 		function disk_handling(item){
+			console.log('5');
 			// determine col and row
 			var col = $(item).attr('id');
 			var row = boardCols[col].length;
@@ -85,29 +89,164 @@
 
 		}
 
+	// function winner_helper(rowOrig, colOrig, playerId){
+	// 	console.log('rowOrig: ' + rowOrig);
+	// 	console.log('colOrig: ' + colOrig);
+	// 	console.log('playerId: ' + playerId);
+
+	// 			boardCols[colOrig].each(function(){
+	// 					console.log(this.getPlayerId().toString())});
+	// 	console.log('boardCols[colOrig]: ' + boardCols[colOrig]);
+	// 	// console.log('boardCols[colOrig][rowOrig]: ' + boardCols[colOrig][rowOrig]);
+	// 	console.log('boardCols[colorOrig][rowOrig].playerId ' +boardCols[colorOrig][rowOrig].playerId);
+
+	// 	return false;
+	// }
+
+		// function checkForWin(disk){
+		// 	playerId = disk.id();
+		// 	return winInDirection(parseInt(disk.row), parseInt(disk.col), 0, 1, playerId) || // check to the right
+		// 	winInDirection(parseInt(disk.row), parseInt(disk.col), 0, -1, playerId) || // check to the left
+		// 	winInDirection(parseInt(disk.row), parseInt(disk.col), -1, 0, playerId) || // check down
+		// 	winInDirection(parseInt(disk.row), parseInt(disk.col), 1, 1, playerId) || // check up and right
+		// 	winInDirection(parseInt(disk.row), parseInt(disk.col), 1, -1, playerId) || // check up and left
+		// 	winInDirection(parseInt(disk.row), parseInt(disk.col), -1, -1, playerId) || // check down and left
+		// 	winInDirection(parseInt(disk.row), parseInt(disk.col), -1, 1, playerId); // check down and right
+
+		// }
+
+		// /*
+		// * Given the current position of the chip, and the directions to change in (dRow, dCol) and the player,
+		// * cheacks if the player has connected 4 in the given direction
+		// */
+		// function winInDirection(currRow, currColumn, dRow, dCol, player){
+		// 	streak = 1;
+
+		// 	while (currColumn + dCol >= 0 && currColumn + dCol < boardCols.length
+		// 	&& currRow + dRow >= 0 && currRow + dRow <= 7
+		// 	&& boardCols[currColumn + dCol].length > (currRow + dRow)
+		// 	&& boardCols[currColumn + dCol][currRow + dRow].id() == player){
+		// 		console.log('while: ' + boardCols[currColumn + dCol][currRow + dRow].id())
+		// 		streak++;
+		// 		currRow = currRow + dRow;
+		// 		currColumn = currColumn + dCol;
+		// 	}
+		// 	return streak >= 4;
+
+		// }
+
+		function winner_helper(rowOrig, colOrig, playerId){
+			console.log('1');
+		var x_streak = 1;
+		for(var i = 1; i < 4; i++){
+			if(x_streak == 4 || (colOrig + i <= 6 && boardCols[colOrig + i].length > rowOrig && boardCols[colOrig + i][rowOrig].id() == playerId)) {
+				x_streak += 1
+				console.log('x_streak: 2a ' + x_streak);
+			} else {
+				console.log('x_streak: 2c ' + x_streak);
+				break;
+			}
+		}
+
+		for(var i = 1; i < 4; i++){
+			if(x_streak == 4 || (colOrig - i >= 0 && boardCols[colOrig - i].length > rowOrig && boardCols[colOrig - i][rowOrig].id() == playerId)) {
+				x_streak += 1
+				console.log('x_streak: 2b ' + x_streak);
+			} else {
+				console.log('x_streak: 2d ' + x_streak);
+				break;
+			}
+		}
+
+		if (x_streak >= 4) {
+			return true;
+		}
+
+		// var y_streak = 1;
+		// for(var i = 1; i < 3; i++){
+		// 	if(y_streak == 4 || (rowOrig - i >= 0 && boardCols[colOrig].length > rowOrig  && boardCols[colOrig][rowOrig - i].id() == playerId)) {
+		// 		y_streak += 1
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+
+		// if (y_streak >= 4) {
+		// 	return true;
+		// }
+
+		// var ld_streak = 1;
+		// for(var i = 1; i < 3; i++){
+		// 	if(ld_streak == 4 || (colOrig - i >= 0 && rowOrig - i >= 0 && boardCols[colOrig - i].length > rowOrig  && boardCols[colOrig - i][rowOrig - i].id() == playerId)) {
+		// 		ld_streak += 1
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+
+		// for(var i = 1; i < 3; i++){
+		// 	if(ld_streak == 4 || (colOrig + i <= 6 && rowOrig + i <= 6 && boardCols[colOrig + i].length > rowOrig  && boardCols[colOrig + i][rowOrig + i].id() == playerId)) {
+		// 		ld_streak += 1
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+
+		// if (ld_streak >= 4) {
+		// 	return true;
+		// }
+
+		// var ul_streak = 1;
+		// for(var i = 1; i < 3; i++){
+		// 	if(ul_streak == 4 || (colOrig - i >= 0 && rowOrig + i <= 6 && boardCols[colOrig - i].length > rowOrig  && boardCols[colOrig - i][rowOrig + i].id() == playerId)) {
+		// 		ul_streak += 1
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+
+		// for(var i = 0; i < 3; i++){
+		// 	if(ul_streak == 4 || (colOrig + i <= 6 && rowOrig - i >= 0 && boardCols[colOrig + i].length > rowOrig  && boardCols[colOrig + i][rowOrig - i].id() == playerId)) {
+		// 		ul_streak += 1
+		// 	} else {
+		// 		break;
+		// 	}
+		// }
+
+		// if (ul_streak >= 4) {
+		// 	console.log('6');
+		// 	return true;
+		// }
+
+		return false;
+	}
+
 
 	// check for winner
 	function isWinnerOrTie(disk){
-
-		// if()
-
+			console.log('7');
+		// left, right, diagonal left down, diagonal right down, regular down.
+		// call winner_helper on each direction to see if there was a win
+		if(winner_helper(parseInt(disk.row), parseInt(disk.col), parseInt(disk.id()))){
+			console.log('entering winner_helper');
+			end_of_game('win', parseInt(disk.id()));
+		}
 		// check if it is a tie
 		if(disks >= 42){
-			end_of_game('tie', disk.playerid);
+			end_of_game('tie', parseInt(disk.id()));
 		} else {
 			return false;
 		}
 	}
 
-	function winner_helper(){
 
-	}
 
-	function end_of_game($reason, $player){
-		if($reason==='tie'){
+	function end_of_game(reason, player){
+			console.log('8');
+		if(reason==='tie'){
 			alert("The game ended in a tie");
-		} else if($reason==='win'){
-			alert("The game ended. " + $player + " won the game");
+		} else if(reason==='win'){
+			alert("The game ended. " + player + " won the game");
 		}
 
 		// handle how to end the game here
@@ -116,6 +255,7 @@
 	}
 
 	function add_disk(row, col, item, from){
+			console.log('9');
 		disks++;
 		var disk = new Disk(userId, row, col);
 		boardCols[col].push(disk);
@@ -146,9 +286,10 @@
 
 	$(function(){
 		$('body').everyTime(2000, function() {
+			console.log('10');
 			var url = "<?= base_url() . 'board/updateMatch'; ?>";
 			$.getJSON(url, function(data, text, jqZHR){
-				if(data && !(data.status)){
+				if(data && !(data.status) && data.row && data.col){
 					var row = data.row;
 					var col = data.col;
 					var col_array = boardCols[col];
